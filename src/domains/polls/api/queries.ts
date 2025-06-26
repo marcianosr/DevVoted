6769e8da363b37e9b31d7f366d0df74427eceb9a
Marciano Schildmeijer <marciano@kabisa.nl>
2025-06-26 14:10:51 +0200

    chore: create fetchPollByIdWithOptions

--- src/domains/polls/api/queries.ts
+++ src/domains/polls/api/queries.ts
@@ -1,17 +1,45 @@
 import { eq } from "drizzle-orm";
-import { pollsTable } from "~/database/schema";
+import { pollOptionsTable, pollsTable } from "~/database/schema";
 import { Poll, pollFactory } from "~/domains/polls/dto";
 import { db } from "~/database/db";
+import { pollOptionFactory } from "../pollOptionsDto";
 
-export const fetchPollById = async (id: number) => {
+export const fetchPollById = async (id: number): Promise<Poll> => {
 	const polls = await db
 		.select()
 		.from(pollsTable)
 		.where(eq(pollsTable.id, id));
 
+	if (!polls.length) {
+		throw new Error("Poll not found");
+	}
+
 	const poll = pollFactory.toDTO(polls[0]);
 
 	return poll;
+};
+
+export const fetchPollByIdWithOptions = async (id: number) => {
+	// TODO: Improve with leftJoin
+	const polls = await db
+		.select()
+		.from(pollsTable)
+		.where(eq(pollsTable.id, id));
+
+	if (!polls.length) {
+		throw new Error("Poll not found");
+	}
+
+	const pollOptions = await db
+		.select()
+		.from(pollOptionsTable)
+		.where(eq(pollOptionsTable.poll_id, id));
+
+	const poll = pollFactory.toDTO(polls[0]);
+
+	const options = pollOptionFactory.toDTOs(pollOptions);
+
+	return { poll, options };
 };
 
 export const fetchAllPolls = async () => {
@@ -20,6 +48,7 @@ export const fetchAllPolls = async () => {
 		.from(pollsTable)
 		.orderBy(pollsTable.created_at);
 
+	// It follows REST API conventions where a GET on a collection returns an empty array, not an error, when no items exist
 	return pollRecords.map((record) => pollFactory.toDTO(record));
 };
 
