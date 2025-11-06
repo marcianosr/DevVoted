70fde567c8f06cb55164d9ba96de9031eb7db980
Marciano Schildmeijer <marciano@kabisa.nl>
2025-11-06 14:54:51 +0100

    chore: setup CI

--- src/database/db.ts
+++ src/database/db.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import "dotenv/config";
 
-export const DATABASE_URL = process.env.DB_URL || "";
+export const DATABASE_URL = process.env.SUPABASE_DB_URL || "";
 
 // Disable prefetch as it is not supported for "Transaction" pool mode
 export const client = postgres(DATABASE_URL, { prepare: false });
