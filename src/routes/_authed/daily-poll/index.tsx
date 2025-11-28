548b1f204b7f9815b60a327c03367da0da98ef2f
Matthijs Groen <matthijs.groen@gmail.com>
2025-11-28 12:16:14 +0100

    Update footer text from 'Tanstack Query' to 'Tanstack Start'

--- src/routes/_authed/daily-poll/index.tsx
+++ src/routes/_authed/daily-poll/index.tsx
@@ -141,7 +141,7 @@ const DailyPoll: React.FC = () => {
 			</section> */}
 			<footer className="p-4 mt-8 bg-zinc-900 text-center text-white">
 				A crazy roguelike obsession build with craftsmanship, passion, ❤️ &
-				Tanstack Query by Marciano Schildmeijer | EST may 2022
+				Tanstack Start by Marciano Schildmeijer | EST may 2022
 			</footer>
 		</section>
 	);
