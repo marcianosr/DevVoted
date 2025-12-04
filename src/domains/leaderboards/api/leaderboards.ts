1a21d8bd8c1f5235a5b3cc4e9eb9503d0e46906c
Marciano Schildmeijer <marciano@kabisa.nl>
2025-12-04 16:12:55 +0100

    fix: prettier

--- src/domains/leaderboards/api/leaderboards.ts
+++ src/domains/leaderboards/api/leaderboards.ts
@@ -5,8 +5,7 @@ import { CategoryCode } from "~/domains/shared/categories";
 export const getLeaderboard = createServerFn({ method: "POST" })
 	.inputValidator((data: { categoryCode?: CategoryCode }) => data)
 	.handler(async ({ data }) => {
-		const { getLiveRunRankingsHandler } = await import(
-			"~/domains/runs/api/handlers"
-		);
+		const { getLiveRunRankingsHandler } =
+			await import("~/domains/runs/api/handlers");
 		return await getLiveRunRankingsHandler(data.categoryCode);
 	});
