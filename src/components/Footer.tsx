044349d1ca16e25429d0d0a75d207271e63c1240
Marciano Schildmeijer <marciano@kabisa.nl>
2025-12-05 12:39:00 +0100

    fix: page layout footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,10 @@
 const Footer = () => (
-	<footer className="p-4 mt-8 bg-zinc-900 text-center text-white">
+	<footer className="p-4 mt-auto bg-zinc-900 text-center text-white">
 		<p>
 			A crazy roguelike obsession build with craftsmanship, passion, ❤️ &
 			Tanstack Start by Marciano Schildmeijer | EST may 2022
 		</p>
-		<p className="mt-2 text-sm text-zinc-400">
+		<p className="mt-2 text-zinc-400">
 			Found a bug?{" "}
 			<a
 				href="https://github.com/marcianosr/DevVoted/issues"
