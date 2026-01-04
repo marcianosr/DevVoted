8c746074f6af07529c5181bd9c637ee3fe889d83
Marciano Schildmeijer <marciano@kabisa.nl>
2026-01-04 19:57:31 +0100

    style: remov padding

--- src/routes/_authed/progress.tsx
+++ src/routes/_authed/progress.tsx
@@ -215,6 +215,8 @@ function RouteComponent() {
 	const { activeConfigs, storageAvailable, storageLimit, storageUsed } =
 		getStorageInfo(activeRun);
 
+	console.log("activeRun", activeRun);
+
 	const deinstallConfigMutation = useMutation({
 		mutationFn: removeConfigFromRunServerFn,
 		onSuccess: () => {
@@ -236,7 +238,7 @@ function RouteComponent() {
 
 	return (
 		<Content poll={dailyPoll.poll}>
-			<section className="max-w-5xl mx-auto p-4 space-y-12">
+			<section className="max-w-5xl mx-auto space-y-12">
 				<PollCountdown />
 
 				<h1 className="text-3xl mb-8">Your progress this run</h1>
@@ -298,13 +300,33 @@ function RouteComponent() {
 				<section className="overflow-hidden">
 					<h3 className="text-3xl">Your active configs</h3>
 					<div className="text-sm text-gray-400">
-						<span>Used: </span>
-						{formatStorage(storageUsed)} / {formatStorage(storageLimit)}
-						{storageAvailable > 0 && (
-							<div className="text-green-600">
-								{formatStorage(storageAvailable)} available
-							</div>
-						)}
+						<span className="text-white">Storage</span>
+						<div className="grid grid-cols-2">
+							<span>what</span>
+							<span>size</span>
+						</div>
+						<div className="grid grid-cols-2">
+							<span>Configs:</span>
+							<span> {formatStorage(storageUsed)}</span>
+							<span>Rebuilds:</span>
+							<span> {formatStorage(activeRun.rerollStorageUsed)}</span>
+							{/* TODO  make sure this also reflects junk from future configs (managed in configManager) */}
+
+							<span>Junk: </span>
+							<span> {formatStorage(activeRun.deinstallPenalty)}</span>
+						</div>
+						<div className="flex gap-2 text-xl">
+							<span className="text-white">
+								Total: {formatStorage(storageUsed)} /{" "}
+								{formatStorage(storageLimit)}
+							</span>
+							<span>·</span>
+							{storageAvailable > 0 && (
+								<span className="text-green-600">
+									{formatStorage(storageAvailable)} available
+								</span>
+							)}
+						</div>
 					</div>
 					<ul className="flex gap-4 overflow-x-auto snap-x snap-mandatory px-4 sm:px-0 py-2">
 						{activeConfigs.length === 0 ? (
