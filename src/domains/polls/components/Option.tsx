0b8026240887929f639ffef1bed637a2f5a7563a
Marciano Schildmeijer <marciano@kabisa.nl>
2026-04-24 20:32:49 +0200

    chore: remove correct

--- src/domains/polls/components/Option.tsx
+++ src/domains/polls/components/Option.tsx
@@ -78,7 +78,6 @@ const Option = ({
 				className={`markdown text-white flex-1 ${disabled ? "cursor-not-allowed text-gray-500" : "cursor-pointer"}`}
 			>
 				<MarkdownText>{option.option}</MarkdownText>
-				Correct: {option.correct ? "Yes" : "No"}
 			</label>
 		</div>
 	);
